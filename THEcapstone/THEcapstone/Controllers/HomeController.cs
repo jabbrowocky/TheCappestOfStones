22b61b593fc636eacbdc8bcb75a9afa29ddc09b6
Jabbrowocky <mindbull3tz@gmail.com>
2018-01-22 10:35:20 -0600

    created user roles

--- THEcapstone/THEcapstone/Controllers/HomeController.cs
+++ THEcapstone/THEcapstone/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using THEcapstone.Models;
 
 namespace THEcapstone.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db;
+        public HomeController()
+        {
+            db = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
+            var test = (from x in db.Users where x.Id == " " select x);
             return View();
         }
 
--- THEcapstone/THEcapstone/Startup.cs
+++ THEcapstone/THEcapstone/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using THEcapstone.Models;
 
 [assembly: OwinStartupAttribute(typeof(THEcapstone.Startup))]
 namespace THEcapstone
@@ -9,6 +12,55 @@ namespace THEcapstone
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "Jabbrowocky";
+                user.Email = "mindbull3tz@gmail.com";
+                string userPWD = "Sk@tin06";
+                var createdUser = userManager.Create(user, userPWD);
+
+                if (createdUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if(!roleManager.RoleExists("Dog Walker"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Dog Walker";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Pet-Sitter"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Pet-Sitter";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Veterinarian"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Veterinarian";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
